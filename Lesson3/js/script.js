59014a685fc748521a3a1ba272225795006a5613
Yauhen <zhendos@tut.by>
2018-10-09 13:13:57 +0200

    commit four 14:13

--- Lesson3/js/script.js
+++ Lesson3/js/script.js
@@ -17,7 +17,7 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     incom: [],
-    savings: false
+    savings: true
 };
 
 function chooseExpenses() {
@@ -39,20 +39,43 @@ function chooseExpenses() {
 
 chooseExpenses();
 
-appData.moneyPerDay = (appData.budget/30).toFixed();
-
-console.log("Ежедневный бюджет : " + appData.moneyPerDay);
-
-if(appData.moneyPerDay < 100) {
-    console.log("Низкий уровень дохода");
-}   else if (appData.moneyPerDay > 100 && appData.moneyPerDay <2000) {
-        console.log("Средний уровень достатка");
-}   else {
-    console.log("Высокий уровень дохода");
+function detectDayBudget(){
+    appData.moneyPerDay = (appData.budget/30).toFixed(4);
+    console.log("Ежедневный бюджет : " + appData.moneyPerDay);
+    alert("Ежедневный бюджет : " + appData.moneyPerDay);
 }
 
+detectDayBudget();
+
+function detectLevel() {
+    if(appData.moneyPerDay < 100) {
+        alert("Низкий уровень дохода");
+    }   else if (appData.moneyPerDay > 100 && appData.moneyPerDay <2000) {
+            alert("Средний уровень достатка");
+    }   else {
+            alert("Высокий уровень дохода");
+    }
+}
+
+detectLevel();
+
+function cooseOptExpenses() {
+    for(let i = 1; i < 4; i++){
+        let a = +prompt("Статья необязательных расходов?", "");
+     if( a != null && a != "" && isNaN(a) == false ){
+        console.log("chooseOpt done!");
+        appData.optionalExpenses[i] = a;
+    } else {
+        alert("Введите корректные данные!!!");
+        i--;
+        }
+    }
+}
+
+cooseOptExpenses();
+
 function checkSavings() {
-    if (appData.savings == true) {
+    if (appData.savings == true ) {
         let save = +prompt("Какова сумма накоплений ?", ""),
             percent = +prompt("Под какой процент?", "");
             appData.mounthIncome = save/100/12*percent;
